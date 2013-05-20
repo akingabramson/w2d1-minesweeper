5bd6c9940e9bdde509728f9b0bf2a9c1fd23cfeb
App Academy Student <student@appacademy.io>
2013-05-20 15:00:25 -0700

    Save/load implemented

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,3 +1,5 @@
+require 'yaml'
+
 class Square
   attr_accessor :adjacent_bombs, :revealed, :flagged, :bomb
 
@@ -11,7 +13,7 @@ class Square
 end
 
 class Game
-  attr_accessor :board, :bomblist
+  attr_accessor :board, :bomblist, :start_time, :end_time
 
   def initialize
     @bomblist = []
@@ -100,17 +102,19 @@ class Game
     move = nil
     location = []
     while true
-      puts "Player, make your choice. [R]eveal or [F]lag (x,y)."
+      puts "Player, make your choice. [R]eveal or [F]lag (x,y). || [S]ave to exit."
       #no invalids, misspellings or off board
+      save_catch = gets.chomp
+      return "s" if save_catch == "s"
 
-      choice = gets.chomp.split(" ")
+      choice = save_catch.chomp.split(" ")
       move = choice[0].downcase
       location = choice[1].split(",")
       location.map!(&:to_i)
       if on_board?(location) == false || !["r", "f"].include?(move)
         puts "Invalid Choice: Bad input. \n\n"
       elsif @board[location[0]][location[1]].flagged && move == "r" || @board[location[0]][location[1]].revealed && move == "f"
-          puts "Invalid Choice: Spot has already been flagged or revealed\n\n"
+        puts "Invalid Choice: Spot has already been flagged or revealed\n\n"
       else
         break
       end
@@ -185,12 +189,13 @@ class Game
   end
 
   def run_game
-    puts "Welcome to Minesweeper. 9x9 or a 16x16 game? [9/16]?"
-    make_board(gets.chomp.to_i)
-    puts "Game start!"
-    print_board
+
     until game_over?
       player_move = get_input
+      if player_move == "s"
+        save_game
+        next
+      end
       apply_move(player_move)
       print_board
     end
@@ -211,12 +216,41 @@ class Game
     print_board
   end
 
-end
+  def begin_game
+    puts "New Game. 9x9 or a 16x16 game? [9/16]?"
+    make_board(gets.chomp.to_i)
+    puts "Game start!"
+    print_board
+    run_game
+  end
 
-# a = Game.new
-# a.make_board(9)
-# a.print_board
-# p a.bomblist
-# a.apply_move(["r",[2,7]])
-# p a.game_over?
-# a.print_board
+  def save_game
+    puts "What do you want to name your save?"
+    filename = gets.chomp
+    a = self.to_yaml
+    savefile = File.open("#{filename}.txt", "w")
+    savefile.puts a
+    savefile.close
+  end
+
+  def admin_panel
+    puts "Welcome to Minesweeper. [N]ew or [L]oad game?"
+    input = gets.chomp.downcase
+    case input
+      when "n"
+        a = Game.new
+        a.begin_game
+      when "l"
+        puts "Enter load file name"
+        filename = gets.chomp
+        loadfile = File.new("#{filename}.txt")
+        loaded_game = YAML::load(loadfile)
+
+        loaded_game.print_board
+        loaded_game.run_game
+      else
+        puts "Invalid answer"
+    end
+  end
+
+end
